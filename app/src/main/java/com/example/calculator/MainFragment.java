6a815946084da2e4a81bf8c6d7ce232f46af443b
Suzuki Takumi <u1tramarine.takumi@gmail.com>
2019-11-17 23:54:50 +0900

    Implement a bit.

--- app/src/main/java/com/example/calculator/MainFragment.java
+++ app/src/main/java/com/example/calculator/MainFragment.java
@@ -1,14 +1,43 @@
 package com.example.calculator;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainFragment extends Fragment {
+
+    private MainViewModel viewModel;
+
+    private TextView textViewFormula;
+
+    private Button button0;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+
+    private Button buttonSlash;
+    private Button buttonAsterisk;
+    private Button buttonPlus;
+    private Button buttonMinus;
+
+    private Button buttonClear;
+    private Button buttonEqual;
 
     public MainFragment() {
     }
@@ -23,8 +52,107 @@ public class MainFragment extends Fragment {
     }
 
     @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        viewModel = MainActivity.obtainViewModel(getActivity());
+        viewModel.formula().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                textViewFormula.setText(s);
+            }
+        });
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews(view);
+    }
+
+    private void initViews(View root) {
+        textViewFormula = root.findViewById(R.id.text_formula);
+
+        button0 = root.findViewById(R.id.button_0);
+        button0.setOnClickListener(new OnNumberClickListener(0));
+        button1 = root.findViewById(R.id.button_1);
+        button1.setOnClickListener(new OnNumberClickListener(1));
+        button2 = root.findViewById(R.id.button_2);
+        button2.setOnClickListener(new OnNumberClickListener(2));
+        button3 = root.findViewById(R.id.button_3);
+        button3.setOnClickListener(new OnNumberClickListener(3));
+        button4 = root.findViewById(R.id.button_4);
+        button4.setOnClickListener(new OnNumberClickListener(4));
+        button5 = root.findViewById(R.id.button_5);
+        button5.setOnClickListener(new OnNumberClickListener(5));
+        button6 = root.findViewById(R.id.button_6);
+        button6.setOnClickListener(new OnNumberClickListener(6));
+        button7 = root.findViewById(R.id.button_7);
+        button7.setOnClickListener(new OnNumberClickListener(7));
+        button8 = root.findViewById(R.id.button_8);
+        button8.setOnClickListener(new OnNumberClickListener(8));
+        button9 = root.findViewById(R.id.button_9);
+        button9.setOnClickListener(new OnNumberClickListener(9));
+
+        buttonSlash = root.findViewById(R.id.button_slash);
+        buttonSlash.setOnClickListener(new OnOperatorClickListener("/"));
+        buttonAsterisk = root.findViewById(R.id.button_asterisk);
+        buttonAsterisk.setOnClickListener(new OnOperatorClickListener("*"));
+        buttonPlus = root.findViewById(R.id.button_plus);
+        buttonPlus.setOnClickListener(new OnOperatorClickListener("+"));
+        buttonMinus = root.findViewById(R.id.button_minus);
+        buttonMinus.setOnClickListener(new OnOperatorClickListener("-"));
+
+        buttonClear = root.findViewById(R.id.button_clear);
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewModel.clear();
+            }
+        });
+        buttonEqual = root.findViewById(R.id.button_equal);
+        buttonEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewModel.calculate();
+            }
+        });
+    }
+
+    private void addNumber(int number) {
+        viewModel.addNumber(number);
+    }
+
+    private void addOperator(String operator) {
+        viewModel.addOperato(operator);
+    }
+
+    private class OnNumberClickListener implements View.OnClickListener {
+        private final int number;
+        OnNumberClickListener(int number) {
+            this.number = number;
+        }
+
+        @Override
+        public void onClick(View view) {
+            addNumber(number);
+        }
+    }
+
+    private class OnOperatorClickListener implements View.OnClickListener {
+        private final String operator;
+        OnOperatorClickListener(String operator) {
+            this.operator = operator;
+        }
+
+        @Override
+        public void onClick(View view) {
+            addOperator(operator);
+        }
     }
 }
--- app/src/main/java/com/example/calculator/MainViewModel.java
+++ app/src/main/java/com/example/calculator/MainViewModel.java
@@ -1,6 +1,50 @@
 package com.example.calculator;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
+
+    private enum State {
+        EMPTY,
+        NUMBER,
+        OPERATOR;
+    }
+
+    private State state = State.EMPTY;
+    private final MutableLiveData<String> formula = new MutableLiveData<>();
+
+    public LiveData<String> formula() {
+        return formula;
+    }
+
+    public void addNumber(int number) {
+        addToFomula(String.valueOf(number));
+        state = State.NUMBER;
+    }
+
+    public void addOperato(String operator) {
+        addToFomula(operator);
+        state = State.OPERATOR;
+    }
+
+    public void clear() {
+        formula.postValue("");
+        state = State.EMPTY;
+    }
+
+    public void calculate() {
+        if (state != State.NUMBER) {
+            return;
+        }
+    }
+
+    private void addToFomula(String s) {
+        String f = formula.getValue();
+        if (f != null) {
+            s = f + s;
+        }
+        formula.postValue(s);
+    }
 }
